4082d92a37fd7d4e2cbbf34451f608673582db48
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-15 10:02:57 +0200

    Checkout folder added.

---
+++ GameShopStoreV2.Core/Items/Checkouts/CheckoutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.Checkouts
+{
+    public class CheckoutViewModel
+    {
+        public int CartId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public string Username { get; set; } = null!;
+        public List<GameViewModel> Listgame { get; set; } = null!;
+    }
+}
