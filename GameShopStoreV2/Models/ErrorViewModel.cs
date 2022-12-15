090d7049431878845923f3f0350bf7075bb173ad
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-15 09:47:04 +0200

    Add project files.

---
+++ GameShopStoreV2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameShopStoreV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
