f95a538b02daf10b4a8ac3e87d181430af0dbe05
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-15 09:55:17 +0200

    Created the classes for Carts folder.

---
+++ GameShopStoreV2.Core/Items/Carts/CreateCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.Carts
+{
+    public class CreateCart
+    {
+        public int GameId { get; set; }
+    }
+}
---
+++ GameShopStoreV2.Core/Items/Carts/ResponseOrderedItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.Carts
+{
+    public class ResponseOrderedItem
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Discount { get; set; }
+        public DateTime DateAdded { get; set; }
+        public List<string> ImageList { get; set; } = null!;
+        public List<string> GenreName { get; set; } = null!;
+        public List<string> GenreIds { get; set; } = null!;
+    }
+}
