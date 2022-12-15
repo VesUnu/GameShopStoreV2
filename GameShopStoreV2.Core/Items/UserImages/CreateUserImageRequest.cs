c16957afe1cad3936ab0a33a26e61d98707e073c
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-15 10:48:40 +0200

    UserImages folder complete

---
+++ GameShopStoreV2.Core/Items/UserImages/CreateUserImageRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.UserImages
+{
+    public class CreateUserImageRequest
+    {
+        public IFormFile ImageFile { get; set; } = null!;
+    }
+}
---
+++ GameShopStoreV2.Core/Items/UserImages/UserImageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.UserImages
+{
+    public class UserImageViewModel
+    {
+        public string ImageName { get; set; } = null!;
+        public DateTime DateUpdated { get; set; }
+    }
+}
