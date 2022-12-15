05b98f01fe15a2c75939895358925bad71ed1dc9
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-15 10:45:29 +0200

    GameImages folder complete.

---
+++ GameShopStoreV2.Core/Items/GameImages/CreateGameImageReceive.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.GameImages
+{
+    public class CreateGameImageReceive
+    {
+        public int GameId { get; set; }
+        public string Caption { get; set; } = null!;
+        public bool isDefault { get; set; }
+        public int SortOrder { get; set; }
+        public IFormFile ImageFile { get; set; } = null!;
+    }
+}
---
+++ GameShopStoreV2.Core/Items/GameImages/GameImageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.GameImages
+{
+    public class GameImageViewModel
+    {
+        public int GameId { get; set; }
+        public int ImageId { get; set; }
+        public string Path { get; set; } = null!;
+        public bool isDefault { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public long FileSize { get; set; }
+        public string Caption { get; set; } = null!;
+        public int SortOrder { get; set; }
+    }
+}
