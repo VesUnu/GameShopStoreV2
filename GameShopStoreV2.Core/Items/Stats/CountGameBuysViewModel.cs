b6ac9d96306f7f3c939fdfe4cd0834afaa4436fa
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-17 09:42:09 +0200

    Added a Stats folder with a class for counting how many times game titles are bought.

---
+++ GameShopStoreV2.Core/Items/Stats/CountGameBuysViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Core.Items.Stats
+{
+    public class CountGameBuysViewModel
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; } = null!;
+        public int BuyCount { get; set; }
+        public decimal Total { get; set; }
+    }
+}
