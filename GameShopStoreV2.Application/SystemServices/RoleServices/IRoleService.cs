e058479b3462a65fa83e07d4c9b6df10d01b5f92
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-18 14:22:49 +0200

    Role services added.

---
+++ GameShopStoreV2.Application/SystemServices/RoleServices/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using GameShopStoreV2.Core.System.Roles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Application.SystemServices.RoleServices
+{
+    public interface IRoleService
+    {
+        Task<List<RoleViewModel>> GetAll();
+    }
+}
