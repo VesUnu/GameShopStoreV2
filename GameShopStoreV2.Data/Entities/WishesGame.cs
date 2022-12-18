ab34cedc510f43d7bf99695cbe3bd127564e09e3
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-18 10:33:15 +0200

    A missing nullable.

--- GameShopStoreV2.Data/Entities/WishesGame.cs
+++ GameShopStoreV2.Data/Entities/WishesGame.cs
@@ -3,7 +3,7 @@
     public class WishesGame
     {
         public int Id { get; set; }
-        public Game Game { get; set; }
+        public Game Game { get; set; } = null!;
         public Wishlist Wishlist { get; set; } = null!;
         public int WishId { get; set; }
         public int GameId { get; set; }
