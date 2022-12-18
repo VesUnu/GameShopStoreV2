313603856d9f6dd39902c9ae14a8fd4524234d00
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-18 10:47:42 +0200

    Added a new project for the services, as well as linking them to the .Core, .Utilities and .Data projects.

---
+++ GameShopStoreV2.Application/ItemServices/Carts/ICartService.cs
@@ -0,0 +1,19 @@
+﻿using GameShopStoreV2.Core.Common;
+using GameShopStoreV2.Core.Items.Carts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStoreV2.Application.ItemServices.Carts
+{
+    public interface ICartService
+    {
+        Task<ResultApi<bool>> AddToCart(string UserID, CreateCart cartCreateRequest);
+
+        Task<ResultApi<List<ResponseOrderedItem>>> GetCart(string UserID);
+
+        Task<ResultApi<bool>> DeleteItem(string UserID, DeleteOrderedItem orderItemDelete);
+    }
+}
