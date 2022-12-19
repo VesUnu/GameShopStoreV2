8b447cddf70e247b513cbf43a2c893e256afc6be
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-19 11:29:02 +0200

    Overhaul of the starting program.

---
+++ GameShopStoreV2/Startup.cs
@@ -0,0 +1,143 @@
+using GameShopStoreV2.Application.CommonServices;
+using GameShopStoreV2.Application.ItemServices.Carts;
+using GameShopStoreV2.Application.ItemServices.Categories;
+using GameShopStoreV2.Application.ItemServices.Checkouts;
+using GameShopStoreV2.Application.ItemServices.Contacts;
+using GameShopStoreV2.Application.ItemServices.Games;
+using GameShopStoreV2.Application.ItemServices.Wishlists;
+using GameShopStoreV2.Application.SystemServices.RoleServices;
+using GameShopStoreV2.Application.SystemServices.UserServices;
+using GameShopStoreV2.Data;
+using GameShopStoreV2.Data.EF;
+using GameShopStoreV2.Data.Entities;
+using GameShopStoreV2.Utilities.Constants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShopStoreV2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<GameShopStoreVTwoDBContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString(SystemConstant.MainConnectionString)));
+            services.AddIdentity<AppUser, AppRole>()
+               .AddEntityFrameworkStores<GameShopStoreVTwoDBContext>()
+               .AddDefaultTokenProviders();
+            services.AddTransient<IGameService, GameService>();
+            services.AddTransient<IStorageService, FileStorageService>();
+            services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+            services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IRoleService, RoleService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<ICartService, CartService>();
+            services.AddTransient<IWishlistService, WishlistService>();
+            services.AddTransient<ICheckoutService, CheckoutService>();
+            services.AddTransient<IContactService, ContactService>();
+            services.AddTransient<IStatsService, ChartService>();
+            //services.AddTransient<IValidator<LoginRequest>, LoginRequestValidator>();
+            services.AddControllers()
+             .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger GameShop Api", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                      }
+                    });
+            });
+            string issuer = Configuration.GetValue<string>("Tokens:Issuer");
+            string signingKey = Configuration.GetValue<string>("Tokens:Key");
+            byte[] signingKeyBytes = System.Text.Encoding.UTF8.GetBytes(signingKey);
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = issuer,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = System.TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
+                };
+            });
+            services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+            {
+                builder.WithOrigins("http://localhost:5000").AllowAnyMethod().AllowAnyHeader();
+                builder.WithOrigins("http://localhost:5003").AllowAnyMethod().AllowAnyHeader();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseCors("ApiCorsPolicy");
+            app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger GameShop Api V1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
