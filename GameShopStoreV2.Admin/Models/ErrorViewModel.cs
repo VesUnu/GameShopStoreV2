3acb846fb5c950ab4017df0d6e3107bd55c51619
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-19 12:29:24 +0200

    Another project for administrator.

---
+++ GameShopStoreV2.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameShopStoreV2.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
