b6a86ce518844034a6baf11162561f104eb403c4
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-19 15:00:30 +0200

    Changing the home page, _layout, adding a Component\Pager folder and assets-demo folder as well as fixing an error with the PagerViewComponent

--- GameShopStoreV2.Admin/Controllers/Components/PagerViewComponent.cs
+++ GameShopStoreV2.Admin/Controllers/Components/PagerViewComponent.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameShopStoreV2.Core.Common;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameShopStoreV2.Admin.Controllers.Components
 {
     public class PagerViewComponent : ViewComponent
     {
-        public Task<IViewComponentResult> InvokeAsync(PagedResultBase result)
+        public Task<IViewComponentResult> InvokeAsync(BasePagedResult result)
         {
             return Task.FromResult((IViewComponentResult)View("Default", result));
         }
